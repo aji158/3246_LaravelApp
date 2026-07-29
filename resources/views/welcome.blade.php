4bd3319182ca81051f53614d2ee61437ce2fefaa
aji158 <anugrahss@students.amikom.ac.id>
2026-07-29 07:17:55 +0700

    fix: change event image variable to poster_path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,9 +104,9 @@
                 </div>
 
                 <!-- Logo Partner -->
-                <img src="{{ \Illuminate\Support\Str::startsWith($partner->logo_path, 'http') ? $partner->logo_path : asset('storage/' . $partner->logo_path) }}" ...>
-                     alt="{{ $partner->name }}"
-                     class="relative z-10 h-24 object-contain grayscale group-hover:grayscale-0 transition duration-500">
+                <img src="{{ \Illuminate\Support\Str::startsWith($partner->logo_path, 'http') ? $partner->logo_path : asset('storage/' . $partner->logo_path) }}" 
+     alt="{{ $partner->name }}"
+     class="relative z-10 h-24 object-contain grayscale group-hover:grayscale-0 transition duration-500">
 
             </div>
 
